f0561f5c13b45c0df73e5367ae9985b9538aa68c
sebsmgzz <sebastian.mgzz@outlook.com>
2022-12-20 16:40:53 -0600

    refact: Move critical logging to factory

--- src/IoTunas.Commands/Factories/CommandHandlerFactory.cs
+++ src/IoTunas.Commands/Factories/CommandHandlerFactory.cs
@@ -3,30 +3,39 @@
 using IoTunas.Extensions.Commands.Collections;
 using IoTunas.Extensions.Commands.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics.CodeAnalysis;
 
 public class CommandHandlerFactory : ICommandHandlerFactory
 {
 
+    public const string InvalidHandlerLog =
+        "Handler for {methodName} needs to " +
+        "implement {interfaceName} and be registered " +
+        "in the service provider's DI to handle a direct method invocation.";
+
+    private readonly CommandHandlerMapping handlersMapping;
     private readonly IServiceProvider serviceProvider;
-    private readonly CommandHandlerMapping handlersDefinition;
+    private readonly ILogger logger;
 
     public CommandHandlerFactory(
+        CommandHandlerMapping handlersMapping,
         IServiceProvider serviceProvider,
-        CommandHandlerMapping handlersDefinitions)
+        ILogger<ICommandHandlerFactory> logger)
     {
+        this.handlersMapping = handlersMapping;
         this.serviceProvider = serviceProvider;
-        this.handlersDefinition = handlersDefinitions;
+        this.logger = logger;
     }
 
     public bool Contains(string methodName)
     {
-        return handlersDefinition.Contains(methodName);
+        return handlersMapping.Contains(methodName);
     }
 
     public bool TryGet(string methodName, [MaybeNullWhen(false)] out ICommandHandler handler)
     {
-        if (!handlersDefinition.TryGetValue(methodName, out var handlerType))
+        if (!handlersMapping.TryGetValue(methodName, out var handlerType))
         {
             handler = null;
             return false;
@@ -34,6 +43,10 @@ public class CommandHandlerFactory : ICommandHandlerFactory
         var scope = serviceProvider.CreateScope();
         var service = scope.ServiceProvider.GetService(handlerType);
         handler = service as ICommandHandler;
+        if(handler == null)
+        {
+            logger.LogCritical(InvalidHandlerLog, methodName, nameof(ICommandHandler));
+        }
         return handler != null;
     }
 
--- src/IoTunas.Commands/Mediators/CommandHandlerMediator.cs
+++ src/IoTunas.Commands/Mediators/CommandHandlerMediator.cs
@@ -12,10 +12,6 @@ public class CommandHandlerMediator : ICommandHandlerMediator
     public const string NotFoundLog = "Not found | {name}";
     public const string HandledLog = "Handled | {name}";
     public const string ErrorLog = "Error | {name}";
-    public const string InvalidHandlerLog =
-        "Handler for {methodRequestName} needs to " +
-        "implement {interfaceName} and be registered " +
-        "in the service provider's DI to handle a direct method invocation.";
 
     private readonly ICommandHandlerFactory handlerFactory;
     private readonly IMethodResponseFactory responseFactory;
@@ -37,25 +33,15 @@ public class CommandHandlerMediator : ICommandHandlerMediator
         try
         {
 
-            // First, check if handler exists
+            // Try to get handler
             logger.LogInformation(InvokedLog, methodRequest.Name);
-            if (!handlerFactory.Contains(methodRequest.Name))
+            if (!handlerFactory.TryGet(methodRequest.Name, out var handler))
             {
                 logger.LogWarning(NotFoundLog, methodRequest.Name);
                 return await HandleNotFoundAsync(methodRequest, userContext);
             }
 
-            // Then try to get it
-            // If the handler is not implementing the interface
-            // or not being registered in the DI service provider
-            // throw an exception, because this should NEVER happen
-            if (!handlerFactory.TryGet(methodRequest.Name, out var handler))
-            {
-                logger.LogCritical(InvalidHandlerLog, methodRequest.Name, nameof(ICommandHandler));
-                return await HandleInvalidHandlerAsync(methodRequest, userContext);
-            }
-
-            // Finally, handle the invocation
+            // Handle the invocation
             logger.LogInformation(HandledLog, methodRequest.Name);
             return await handler.HandleAsync(methodRequest, userContext);
 
@@ -73,16 +59,6 @@ public class CommandHandlerMediator : ICommandHandlerMediator
         return await Task.FromResult(responseFactory.NotFound(new
         {
             status = "not found",
-            name = methodRequest.Name
-        }));
-    }
-
-    protected virtual async Task<MethodResponse> HandleInvalidHandlerAsync(
-        MethodRequest methodRequest, object userContext)
-    {
-        return await Task.FromResult(responseFactory.InternalError(new
-        {
-            status = "invalid handler",
             name = methodRequest.Name
         }));
     }
