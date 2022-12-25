d6f1c129de28b1959b75295916711a8b53c22138
sebsmgzz <sebastian.mgzz@outlook.com>
2022-12-24 18:47:23 -0600

    fix: Include service provider extensions

---
+++ src/IoTunas.Core/Hosting/IServiceProviderExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace IoTunas.Core.Hosting;
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+public static class IServiceProviderExtensions
+{
+
+    public static bool TryGetService(
+        this IServiceProvider provider,
+        Type serviceType,
+        [MaybeNullWhen(false)] out object service)
+    {
+        using var scope = provider.CreateScope();
+        service = scope.ServiceProvider.GetService(serviceType);
+        return service != null;
+    }
+
+    public static bool TryGetService<TType>(
+        this IServiceProvider provider,
+        Type serviceType,
+        [MaybeNullWhen(false)] out TType service)
+        where TType : class
+    {
+        var isCreated = provider.TryGetService(serviceType, out var serviceObj);
+        service = isCreated ? serviceObj as TType : null;
+        return service != null;
+    }
+
+    public static bool TryGetService<TType>(
+        this IServiceProvider provider,
+        [MaybeNullWhen(false)] out TType service)
+        where TType : class
+    {
+        return provider.TryGetService(typeof(TType), out service);
+    }
+
+}
