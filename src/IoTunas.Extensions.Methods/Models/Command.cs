ae14a2c38169709edd5e70217cf7a4acb1732da8
sebsmgzz <sebastian.mgzz@outlook.com>
2022-12-26 15:17:39 -0600

    refact: Use Newtonsoft for jsons

--- src/IoTunas.Extensions.Methods/Models/Command.cs
+++ src/IoTunas.Extensions.Methods/Models/Command.cs
@@ -10,11 +10,15 @@ public abstract class Command<T> : ICommand
     public async Task<MethodResponse> HandleAsync(
         MethodRequest methodRequest, object userContext)
     {
-        var payloadModel = JsonConvert.DeserializeObject<T>(methodRequest.DataAsJson);
+        using var dataStream = new MemoryStream(methodRequest.Data);
+        using var streamReader = new StreamReader(dataStream);
+        using var jsonReader = new JsonTextReader(streamReader);
+        var serializer = new JsonSerializer();
+        var payloadModel = serializer.Deserialize<T>(jsonReader);
         return await HandleAsync(payloadModel, userContext);
     }
 
     public abstract Task<MethodResponse> HandleAsync(
-        T payloadModel, object userContext);
+        T? payloadModel, object userContext);
 
 }
--- src/IoTunas.Extensions.Telemetry/Models/Receiver.cs
+++ src/IoTunas.Extensions.Telemetry/Models/Receiver.cs
@@ -1,14 +1,17 @@
 ﻿namespace IoTunas.Extensions.Telemetry.Models;
 
 using Microsoft.Azure.Devices.Client;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 public abstract class Receiver<T> : IReceiver
 {
 
     public async Task<MessageResponse> HandleAsync(Message message, object userContext)
     {
-        var payloadModel = JsonSerializer.Deserialize<T>(message.BodyStream);
+        using var streamReader = new StreamReader(message.BodyStream);
+        using var jsonReader = new JsonTextReader(streamReader);
+        var serializer = new JsonSerializer();
+        var payloadModel = serializer.Deserialize<T>(jsonReader);
         return await HandleAsync(payloadModel, userContext);
     }
 
