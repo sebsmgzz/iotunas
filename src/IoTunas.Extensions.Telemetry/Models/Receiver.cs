500d16e185ef3d9eeb7a5c153d55b94472edfc1f
sebsmgzz <sebastian.mgzz@outlook.com>
2022-12-26 15:08:36 -0600

    feat: Add generic receiver

---
+++ src/IoTunas.Extensions.Telemetry/Models/Receiver.cs
@@ -0,0 +1,18 @@
+﻿namespace IoTunas.Extensions.Telemetry.Models;
+
+using Microsoft.Azure.Devices.Client;
+using System.Text.Json;
+
+public abstract class Receiver<T> : IReceiver
+{
+
+    public async Task<MessageResponse> HandleAsync(Message message, object userContext)
+    {
+        var payloadModel = JsonSerializer.Deserialize<T>(message.BodyStream);
+        return await HandleAsync(payloadModel, userContext);
+    }
+
+    public abstract Task<MessageResponse> HandleAsync(T? model, object userContext);
+
+
+}
